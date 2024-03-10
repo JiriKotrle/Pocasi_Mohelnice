f59d130e5fdc69f9e70fda91e2276177900b36f9
JiriKotrle <jirikotrle@gmail.com>
2024-03-10 19:21:28 +0100

    10.3.2024

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import os
+from matplotlib import pyplot as plt
+import numpy as np
 
 os.system('cls')
 
@@ -20,14 +22,30 @@ def update_pocasi_csv(day, avg_temp, avg_prec):
     new_df = pd.DataFrame(all_data, columns=df.columns)
     print(new_df)
 
-# Spojení původního DataFrame a nového DataFrame
+    # Spojení původního DataFrame a nového DataFrame
     df = pd.concat([df, new_df], ignore_index=True)
 
-# Uložení aktualizovaného DataFrame zpět do souboru CSV
+    # Uložení aktualizovaného DataFrame zpět do souboru CSV
     df.to_csv("pocasi.csv", index=False, sep='\t', encoding='cp1250', decimal=',')
+    print(len(df))
+    # Aktualizace souboru CSV
+# update_pocasi_csv(day, avg_temp, avg_prec)
 
-# Aktualizace souboru CSV
-update_pocasi_csv(day, avg_temp, avg_prec)
+def plot_chart():
+    x_values = ["0-4","5-8", "9-12", "13-16", "17-20", "21-24"]
+    df = pd.read_csv("pocasi.csv", sep='\t', encoding='cp1250',decimal=',')
+    y_values_temp = []
+    # projede řádky
+    for i in range(len(df)):
+        # projede sloupce:
+        for ii in range(6):
+            temp = df.iloc[i,ii+1]
+            y_values_temp.append(temp)
+    print(y_values_temp)
+    x_values_extended = np.tile(x_values, 3)
 
+    plt.plot(x_values_extended,y_values_temp)
+    plt.show()
+plot_chart()
 
 
