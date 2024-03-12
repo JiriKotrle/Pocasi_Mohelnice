cecc129beab93a4635907cd197202bd02151562c
JiriKotrle <jirikotrle@gmail.com>
2024-03-12 19:13:27 +0100

    12.03.2024

--- pocasi.py
+++ pocasi.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 import pandas as pd
 import os
+from matplotlib import pyplot as plt
+from matplotlib.widgets import Slider  # Import Slider
+
 
 os.system('cls')
 
@@ -136,9 +139,65 @@ def update_pocasi_csv(day, avg_temp, avg_prec):
     df.to_csv("pocasi.csv", index=False, sep='\t', encoding='cp1250', decimal=',')
 
 
-# do try přijde update .csv pokud zahlásí chybu, že neexistuje, tak se půjde do větve except, kde se csv vytvoří.
-# try:
-# except:
+def plot_chart():
+    df = pd.read_csv("pocasi.csv", sep='\t', encoding='cp1250',decimal=',')
+
+    y_values_temp = []
+    # projede řádky
+    for i in range(len(df)):
+        # projede sloupce:
+        for ii in range(6):
+            temp = df.iloc[i,ii+1]
+            y_values_temp.append(temp)
+    print(y_values_temp)
+
+    temp_columns_name = ['temp (0-4)', 'temp (5-8)', 'temp (9-12)', 'temp(13-16)', 'temp(17-20)', 'temp(21-24)']
+    prec_columns_name = ['prec(0-4)', 'prec(5-8)', 'prec(9-12)', 'prec(13-16)', 'prec(17-20)', 'prec(21-24)']
+
+    list_datums = df['datum'].tolist()
+    print(list_datums)
+
+    x_values = []
+    index = 1
+
+    for i in list_datums:
+        for ii in temp_columns_name:
+            x_value = " ".join([f'({str(index)})',ii,i])
+            x_values.append(x_value)
+            index += 1
+    print(x_values)
+
+
+    fig, ax = plt.subplots()
+
+    plt.plot(x_values, y_values_temp, marker='x')
+    
+    # Nastavení úhlu natočení názvů na ose x a velikosti písma
+    plt.xticks(rotation=90, fontsize=6)
+
+    # Posunutí okrajů grafu nahoru
+    plt.subplots_adjust(bottom=0.3)
+
+    # Popisky os
+    plt.xlabel('Osa X')
+    plt.ylabel('Osa Y')
+
+    # Název grafu
+    plt.title('Opakované hodnoty na ose X')
+
+    # Vytvoření slideru
+    axcolor = 'lightgoldenrodyellow'
+    ax_slider = plt.axes([0.1, 0.1, 0.65, 0.03], facecolor=axcolor)
+    slider = Slider(ax_slider, 'Index', 1, len(x_values), valinit=1)
+
+    def update(val):
+        index = int(slider.val)
+        ax.set_xlim(index - 10, index + 10)  # Updatuje rozsah zobrazených hodnot
+        fig.canvas.draw_idle()  # Překreslí graf po změně
+
+    slider.on_changed(update)
+    # Zobrazení grafu
+    plt.show()
 
 def get_result():
     url_teplota = get_url_temp()
@@ -154,7 +213,7 @@ def get_result():
         update_pocasi_csv(day, avg_temp, avg_prec)
     except FileNotFoundError:
         create_pocasi_csv(day, avg_temp, avg_prec)
-    
+    plot_chart()
 
 
 get_result()
--- test2.py
+++ test2.py
@@ -2,15 +2,13 @@ import pandas as pd
 import os
 from matplotlib import pyplot as plt
 import numpy as np
+from matplotlib.widgets import Slider  # Import Slider
 
 os.system('cls')
 
 
 def plot_chart():
     df = pd.read_csv("pocasi.csv", sep='\t', encoding='cp1250',decimal=',')
-
-    x_values = 3*[0,4,8,12,16,20]
-    print(x_values)
 
     y_values_temp = []
     # projede řádky
@@ -19,8 +17,32 @@ def plot_chart():
         for ii in range(6):
             temp = df.iloc[i,ii+1]
             y_values_temp.append(temp)
+    print(y_values_temp)
 
-    plt.plot(range(len(x_values)), y_values_temp)
+    temp_columns_name = ['temp (0-4)', 'temp (5-8)', 'temp (9-12)', 'temp(13-16)', 'temp(17-20)', 'temp(21-24)']
+    prec_columns_name = ['prec(0-4)', 'prec(5-8)', 'prec(9-12)', 'prec(13-16)', 'prec(17-20)', 'prec(21-24)']
+
+    list_datums = df['datum'].tolist()
+    print(list_datums)
+
+    x_values = []
+    index = 1
+
+    for i in list_datums:
+        for ii in temp_columns_name:
+            x_value = " ".join([f'({str(index)})',ii,i])
+            x_values.append(x_value)
+            index += 1
+    print(x_values)
+
+    fig, ax = plt.subplots()
+    plt.plot(x_values, y_values_temp, marker='x')
+    
+    # Nastavení úhlu natočení názvů na ose x a velikosti písma
+    plt.xticks(rotation=90, fontsize=6)
+
+    # Posunutí okrajů grafu nahoru
+    plt.subplots_adjust(bottom=0.3)
 
 # Popisky os
     plt.xlabel('Osa X')
@@ -29,7 +51,18 @@ def plot_chart():
 # Název grafu
     plt.title('Opakované hodnoty na ose X')
 
-# Zobrazení grafu
+    # Vytvoření slideru
+    axcolor = 'lightgoldenrodyellow'
+    ax_slider = plt.axes([0.1, 0.1, 0.65, 0.03], facecolor=axcolor)
+    slider = Slider(ax_slider, 'Index', 1, len(x_values), valinit=1)
+
+    def update(val):
+        index = int(slider.val)
+        ax.set_xlim(index - 10, index + 10)  # Updatuje rozsah zobrazených hodnot
+        fig.canvas.draw_idle()  # Překreslí graf po změně
+
+    slider.on_changed(update)
+    # Zobrazení grafu
     plt.show()
 
 plot_chart()
